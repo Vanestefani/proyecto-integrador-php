3d7468f7f11de4195b912871d9c0da4e9de74b24
vanessa <vanessa.estefania.corredor@gmail.com>
2020-11-05 23:47:33 -0500

    conexion

--- modelo/daoRegistroUsuario.php
+++ modelo/daoRegistroUsuario.php
@@ -3,12 +3,11 @@ include_once 'claseConexion.php';
 include_once 'claseRegistroUsuario.php';
 
 class daoRegistroUsuario {
-    
+
     public static function ingresarDato($p) {
         $con=new claseConexion();
-        $sql="INSERT INTO `usuario`(`idUsuario`, `nombre`, `celular`, `email`, `password`, `fecha_creacion`, `privilegio`, `foto`, `verificacion`, `modo`) "
-                . "VALUES (''"
-                . ",'".$p->getNombre()."'"
+        $sql="INSERT INTO `usuario`( `nombre`, `celular`, `email`, `password`, `fecha_creacion`, `privilegio`, `foto`, `verificacion`, `modo`) "
+                . "VALUES ('".$p->getNombre()."'"
                 . ",'".$p->getCelular()."'"
                 . ",'".$p->getEmail()."'"
                 . ",'".$p->getPassword()."'"
@@ -16,6 +15,7 @@ class daoRegistroUsuario {
                 . ",'".$p->getFoto()."'"
                 . ",''"
                 . ",'')";
+        print(sql);
         $con->ejecutarActualizacion($sql);
         $con->cerrarConexion();{
     }
